af8fc717beff6d5d39a70512b1395d54252bb836
TacioDaito <taciokikuchi@gmail.com>
2023-05-07 20:58:41 -0300

    2058tacio: One-to-many CRUD done

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Models\User;
+use App\Models\Post;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,4 +17,53 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::get('/create/{id}/{title}/{body}', function ($id, $title, $body) {
+    
+    $postAttributes = ['title' => $title, 'body' => $body];
+    $post = Post::create($postAttributes);
+    $user = User::findOrFail($id);
+    $user->posts()->save($post);
+
+    echo 'Body: ' . $post->body . '<br>';
+    echo 'Title: ' . $post->title . '<br>';
+    echo 'Post id: ' . $post->id . '<br>';
+
+});
+
+Route::get('/read/{id}', function ($id) {
+    
+    $user = User::findOrFail($id);
+
+    foreach ($user->posts as $post) {
+        echo '---------------------------<br>';
+        echo 'Title: ' . $post->title . '<br>';
+        echo 'Body: ' . $post->body . '<br>';
+        echo 'Post id: ' . $post->id . '<br>';
+    }
+    
+});
+
+Route::get('/update/{user_id}/{post_id}/{title}/{body}', function ($user_id, $post_id, $title, $body) {
+
+    $user = User::findOrFail($user_id);
+    $post = $user->posts()->whereId($post_id);
+    $postAttributes = ['title' => $title, 'body' => $body];
+    $post->update($postAttributes);
+
+    echo 'Title: ' . $title . '<br>';
+    echo 'Body: ' . $body . '<br>';
+    echo 'Post id: ' . $post_id . '<br>';
+
+});
+
+Route::get('/delete/{user_id}/{post_id}', function ($user_id, $post_id) {
+    
+    $user = User::findOrFail($user_id);
+    $post = $user->posts()->whereId($post_id);
+    $post->delete();
+
+    return redirect('/read/' . $user_id);
+    
 });
